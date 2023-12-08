89521feecce90f714f1ab25cf9caee498c77ecf4
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-12-08 14:45:49 -0500

    Remove all unnecessary comments

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,31 +6,6 @@ import PhotoFavButton from "./PhotoFavButton";
 const PhotoListItem = (props) => {
   const { photo, favPhotos, setFavPhotos, onPhotoClicked, onLikeClicked } = props;
 
-  // let output = photos.map(data => {
-  //   return (
-  //     <li key={data.id}>
-  //       <div className="photo-list__item" >
-  //         <PhotoFavButton
-  //           key={data.id}
-  //           favPhotos={favPhotos}
-  //           setFavPhotos={setFavPhotos}
-  //           photoId={data.id}
-  //           onLikeClicked={onLikeClicked}
-
-  //         />
-  //         <img src={data.urls.regular} className="photo-list__image" onClick={() => onPhotoClicked({data})} name="imageSource" />
-  //         <div className="photo-list__user-details">
-  //           <img src={data.user.profile} className="photo-list__user-profile" />
-  //           <div className="photo-list__user-text">
-  //             <span className="photo-list__user-info">{data.user.username}</span>
-  //             <span className="photo-list__user-location">{data.location.city}, {data.location.country}</span>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </li>
-  //   );
-  // });
-
   return (
     <li key={photo.id}>
       <div className="photo-list__item" >
